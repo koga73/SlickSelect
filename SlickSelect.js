a759aee2f12d16d5563fb32017f29a5e7caa6d38
AJ Savino <aj.savino@moxieusa.com>
2015-06-10 15:34:03 -0400

    updated version

--- SlickSelect.js
+++ SlickSelect.js
@@ -1,5 +1,5 @@
 /*
-* SlickSelect v1.1.1 Copyright (c) 2014 AJ Savino
+* SlickSelect v1.1.2 Copyright (c) 2014 AJ Savino
 * 
 * Permission is hereby granted, free of charge, to any person obtaining a copy
 * of this software and associated documentation files (the "Software"), to deal
